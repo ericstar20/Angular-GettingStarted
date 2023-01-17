641e70d31585dbf6370c341db9ac43a6de636b06
Eric Wang <eric4781cool@gmail.com>
2023-01-16 20:22:03 -0600

    add custome pipe and filter features

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe
   ],
   imports: [
     BrowserModule,
