79aff093acddcbbd656938d04c0d7038f3881aac
Eric Wang <eric4781cool@gmail.com>
2023-01-20 19:51:48 -0600

    routing set up

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,18 +7,30 @@ import { HttpClientModule} from '@angular/common/http'
 import { ProductListComponent } from './product/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'product', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])    
   ],
   bootstrap: [AppComponent]
 })
