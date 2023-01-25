3a071e91a1d0a33da9f167bf647f24939364c10e
Eric Wang <eric4781cool@gmail.com>
2023-01-24 21:30:27 -0600

    built share module & product module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,41 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http'
-
-import { ProductListComponent } from './product/product-list.component';
-import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './product/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './product/product-detail.guard';
+import { ProductModule } from './product/product.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacePipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'product', component: ProductListComponent },
-      { 
-        path: 'product/:id', 
-        canActivate:[ProductDetailGuard], 
-        component: ProductDetailComponent 
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])    
+    ]),
+    ProductModule,
+    SharedModule    
   ],
   bootstrap: [AppComponent]
 })
---
+++ APM-Start/src/app/product/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacePipe } from '../shared/convert-to-space.pipe';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacePipe
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: 'product', component: ProductListComponent },
+      { 
+        path: 'product/:id', 
+        canActivate:[ProductDetailGuard], 
+        component: ProductDetailComponent 
+      }
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule { }
